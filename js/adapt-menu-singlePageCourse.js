2e5080e33769d11f5b88a202750ef2e55622c944
Adam Laird <adam.laird@mps.org.uk>
2014-03-13 15:34:59 +0000

    changed if to === instead of ==

--- js/adapt-menu-singlePageCourse.js
+++ js/adapt-menu-singlePageCourse.js
@@ -4,24 +4,24 @@ define(function (require) {
     var Adapt = require('coreJS/adapt');
     var MenuView = require('coreViews/menuView');
     
-    if(this.model.getChildren().length == 1){
-    Adapt.on('router:menu', function (model) {
+    if (this.model.getChildren().length === 1) {
+        Adapt.on('router:menu', function (model) {
         // Listen to when Adapt hits a menu and navigate to a page. Make sure the string here is the page ID you're trying to route too.
-        var newRouteId = this.model.get('_id')
+            var newRouteId = this.model.get('_id');
 
-        Backbone.history.navigate('#/id/' + newRouteId, true);
+            Backbone.history.navigate('#/id/' + newRouteId, true);
 
-    });
+        });
 
-    Adapt.on('router:page', function (model) {
+        Adapt.on('router:page', function (model) {
 
         // Hide the navigation back button. _.defer makes show everything is rendered before hiding.
 
-        _.defer(function() {
-            $('.navigation-back-button').addClass('display-none');
-            $('.navigation-drawer-toggle-button').addClass('display-none');
+            _.defer(function () {
+                $('.navigation-back-button').addClass('display-none');
+                $('.navigation-drawer-toggle-button').addClass('display-none');
+            });
         });
-    });
     }
 }
-);
+    );
